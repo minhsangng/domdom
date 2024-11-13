2c9dd766c96d17f8ec4a16ecb2190babfda3afa0
Nguyễn Minh Sang <168519551+minhsangng@users.noreply.github.com>
2024-11-13 11:16:30 +0700

    Merge QLNV

--- model/connect.php
+++ model/connect.php
@@ -3,7 +3,7 @@ class Database
 {
     function connect()
     {
-        $conn = new mysqli("localhost", "root", "", "cosodulieu");
+        $conn = new mysqli("localhost", "root", "", "domdom");
 
         if ($conn->connect_error)
             echo "Kết nối database thất bại!";
